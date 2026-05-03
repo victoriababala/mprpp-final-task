7c179753b8be6929217fc7eba44af13e21807d8b
victoriababala <victoriababala1106@gmail.com>
2026-05-03 16:08:57 +0300

    тест з виправленим форматуванням

--- order_service/main.py
+++ order_service/main.py
@@ -9,9 +9,11 @@ app = FastAPI(title=f"Order Service N{STUDENT_N}")
 INVENTORY_URL = "http://inventory-service:8000"
 ORDERS = []
 
+
 class OrderRequest(BaseModel):
     item_id: int
     quantity: int
+
 
 @app.post("/orders")
 def create_order(order: OrderRequest):
@@ -20,16 +22,19 @@ def create_order(order: OrderRequest):
         # Синхронний запит до Inventory Service
         response = requests.get(f"{INVENTORY_URL}/stock/{order.item_id}")
     except requests.exceptions.ConnectionError:
-        raise HTTPException(status_code=503, detail="Inventory Service is unavailable")
+        raise HTTPException(status_code=503,
+                            detail="Inventory Service is unavailable")
 
     if response.status_code == 404:
-        raise HTTPException(status_code=400, detail="Item does not exist")
+        raise HTTPException(status_code=400,
+                            detail="Item does not exist")
 
     stock_data = response.json()["data"]
 
     # Перевірка наявності на складі
     if stock_data["quantity"] < order.quantity:
-        raise HTTPException(status_code=400, detail="Not enough stock")
+        raise HTTPException(status_code=400,
+                            detail="Not enough stock")
 
     new_order = {
         "order_id": STUDENT_N * 100 + len(ORDERS) + 1,
@@ -39,6 +44,7 @@ def create_order(order: OrderRequest):
     }
     ORDERS.append(new_order)
     return {"student_id": STUDENT_N, "data": new_order}
+
 
 @app.get("/orders")
 def get_orders():
