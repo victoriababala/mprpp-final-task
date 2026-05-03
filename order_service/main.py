6130c15d3d7e10655fa7e5e4a6b739defa9af2e8
𝐕𝐈𝐂 <91181739+victoriababala@users.noreply.github.com>
2026-05-03 15:51:52 +0300

    formatting fix

--- order_service/main.py
+++ order_service/main.py
@@ -22,16 +22,19 @@ def create_order(order: OrderRequest):
         # Синхронний запит до Inventory Service
         response = requests.get(f"{INVENTORY_URL}/stock/{order.item_id}")
     except requests.exceptions.ConnectionError:
-        raise HTTPException(status_code=503, detail="Inventory Service is unavailable")
+        raise HTTPException(status_code=503,
+                            detail="Inventory Service is unavailable")
 
     if response.status_code == 404:
-        raise HTTPException(status_code=400, detail="Item does not exist")
+        raise HTTPException(status_code=400,
+                            detail="Item does not exist")
 
     stock_data = response.json()["data"]
 
     # Перевірка наявності на складі
     if stock_data["quantity"] < order.quantity:
-        raise HTTPException(status_code=400, detail="Not enough stock")
+        raise HTTPException(status_code=400,
+                            detail="Not enough stock")
 
     new_order = {
         "order_id": STUDENT_N * 100 + len(ORDERS) + 1,
